92e1f937c8bc17bcc7f7857b41610acb99592b7d
GuilhermeMosson <guimosson@gmail.com>
2024-03-07 22:59:22 -0300

    terminar css do carrinho

--- Carrinho/script.js
+++ Carrinho/script.js
@@ -9,13 +9,17 @@ function exibirItensNoCarrinho() {
         novoItem.textContent = `${item.nome}  -  R$${item.preco}`;
         carrinho.appendChild(novoItem);
       });
+      const total = carrinhoItens.reduce((acc, item) => acc + item.preco, 0);
+      const totalElemento = document.getElementById('total');
+      totalElemento.textContent = `Total do carrinho: R$${total}`;
     }  
 
-function calculartotal(){
-    const carrinhoItens = JSON.parse(localStorage.getItem('carrinho')) || [];
-    const total = carrinhoItens.reduce((acc, item) => acc + item.preco, 0);
-    const totalElemento = document.getElementById('total');
-    totalElemento.textContent = `Total do carrinho R$${total}`;
+function enviarWats(){
+    const carrinhoItens = document.getElementById('carrinho').innerHTML;
+    const totalElemento = document.getElementById('total').innerHTML;
+    var numeroTel = "554198799999"; /*Lembrar de colocar o nº certo*/
+    var linkWhatsApp = "https://wa.me/" + numeroTel + "?text=Produtos: " + carrinhoItens + totalElemento;
+    window.open(linkWhatsApp, "_blank");/*Abre o zap em outra janela*/
 }
 
 function limpar(){
