4aef070527295c5f4d2e5736328dd8db3056eecb
GuilhermeMosson <guimosson@gmail.com>
2024-03-10 19:08:19 -0300

    termino do site

--- Carrinho/script.js
+++ Carrinho/script.js
@@ -15,17 +15,41 @@ function exibirItensNoCarrinho() {
     }  
 
 function enviarWats(){
-    const carrinhoItens = document.getElementById('carrinho').innerHTML;
-    const totalElemento = document.getElementById('total').innerHTML;
-    var numeroTel = "554198799999"; /*Lembrar de colocar o nº certo*/
-    var linkWhatsApp = "https://wa.me/" + numeroTel + "?text=Produtos: " + carrinhoItens + totalElemento;
-    window.open(linkWhatsApp, "_blank");/*Abre o zap em outra janela*/
+    let mensagem = 'Produtos no carrinho: \n';
+    const carrinhoItens = JSON.parse(localStorage.getItem('carrinho')) || [];
+    const total = document.getElementById('total').innerHTML;
+
+    var registro  = new Date();
+    var dt = formatarData(registro);
+
+    carrinhoItens.forEach(item =>  {
+        mensagem += `${item.nome} - R$${item.preco} | `;
+    });
+
+    mensagem += `${total} | Pedido feito em ${dt}`;
+        
+    var numeroTel = "5541999999999"; /*Lembrar de colocar o nº certo*/
+    var linkWhatsApp = "https://wa.me/" + numeroTel + "?text=" + mensagem;
+    window.open(linkWhatsApp, "_blank");/*Abre o zap em outra janela*/;
 }
 
 function limpar(){
     document.getElementById('carrinho').innerHTML = '';
     document.getElementById('total').innerHTML = '';
     localStorage.removeItem('carrinho');
+}
+
+function formatarData(item){
+    var options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    }
+
+    return item.toLocaleString("pt-BR", options)
 }
 
 exibirItensNoCarrinho();
